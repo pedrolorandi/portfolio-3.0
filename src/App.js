b2da0d7a0b6282f0dc3c6ebee3dee2451592e125
pedrolorandi <pedrokl@hotmail.com>
2024-06-29 18:42:57 -0400

    add background-overlay

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import "./App.css";
+import projects from "./data/projects.json";
 
 function App() {
   return (
     <div className="banner">
-      <div className="slider" style={{ "--quantity": 10 }}>
-        <div className="item" style={{ "--position": 1 }}>
-          <img src="https://wallpapercave.com/wp/wp3255752.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 2 }}>
-          <img src="https://wallpapercave.com/wp/wp3255771.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 3 }}>
-          <img src="https://wallpapercave.com/wp/wp3255713.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 4 }}>
-          <img src="https://wallpapercave.com/wp/wp3255783.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 5 }}>
-          <img src="https://wallpapercave.com/wp/wp3255811.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 6 }}>
-          <img src="https://wallpapercave.com/wp/wp3255736.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 7 }}>
-          <img src="https://wallpapercave.com/wp/wp3255733.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 8 }}>
-          <img src="https://wallpapercave.com/wp/wp3255394.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 9 }}>
-          <img src="https://wallpapercave.com/wp/wp3255333.jpg" />
-        </div>
-        <div className="item" style={{ "--position": 10 }}>
-          <img src="https://wallpapercave.com/wp/wp3255831.jpg" />
-        </div>
+      <div className="background-overlay"></div>
+      <div className="slider" style={{ "--quantity": projects.length }}>
+        {projects.map((project, index) => (
+          <div className="item" style={{ "--position": index + 1 }}>
+            <img src={project.video} />
+          </div>
+        ))}
       </div>
     </div>
   );
